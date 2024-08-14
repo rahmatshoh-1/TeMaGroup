340d10d147e4a4af9596a90b0d324a6174714e65
rahmatshoh rahmonov <rahmatshoh.rahmonov@mail.ru>
2024-08-14 13:20:02 +0500

    Fixed swiper

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     slidesPerView: 1,
     loop: true,
+    centeredSlides: false,
         freeMode: true,
     // freeMode:true,
     // If we need pagination
@@ -15,6 +16,24 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+    autoplay: {
+      delay: 0.5,
+      disableOnInteraction: false,
+       },
   });
 
 let menu = document.getElementById('menu').addEventListener('click',toggleCloseMenu)
